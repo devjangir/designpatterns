04dfe7e403be05caae1d94e0e629f37c8298c7f1
devjangir <devpassion7@gmail.com>
2024-03-07 22:31:31 +0530

    Singleton using enum

---
+++ designpatterns/src/main/java/org/example/creational/SingletonUsingEnum.java
@@ -0,0 +1,26 @@
+package org.example.creational;
+
+enum DLoggerEnum {
+    INSTANCE;
+
+    int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
+public class SingletonUsingEnum {
+    public static void main(String[] args) {
+        DLoggerEnum logger = DLoggerEnum.INSTANCE;
+        DLoggerEnum logger2 = DLoggerEnum.INSTANCE;
+        logger.setValue(2);
+        logger2.setValue(3);
+        System.out.println(logger.getValue());
+        System.out.println(logger2.getValue());
+        System.out.println(logger == logger2);
+    }
+}
