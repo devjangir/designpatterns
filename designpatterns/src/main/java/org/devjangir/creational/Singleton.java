6e1970c717ca4a0e911db2c605c0c7844a28370a
devjangir <devpassion7@gmail.com>
2024-03-11 13:06:53 +0530

    Singleton design pattern using double locking

---
+++ designpatterns/src/main/java/org/devjangir/creational/Singleton.java
@@ -0,0 +1,32 @@
+package org.devjangir.creational;
+
+class DLogger {
+
+    private static DLogger instance;
+    private DLogger() {}
+
+    public static DLogger getInstance() {
+        if(instance == null) {
+            // add another check for double checking locking
+            synchronized (DLogger.class) {
+                if(instance == null) {
+                    instance = new DLogger();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println(message);
+    }
+}
+public class Singleton {
+    public static void main(String[] args) {
+        DLogger logger = DLogger.getInstance();
+        DLogger logger2 = DLogger.getInstance();
+        logger.log("Hello world!");
+        logger2.log("Hello world!");
+        System.out.println(logger == logger2);
+    }
+}
